8ecf912b967b96e57128df676b0996f369c28531
Zane Borkar <zane.borkar.25@bishops.com>
2023-01-17 14:12:44 -0800

    this may be my first commit but im pretty far along. the reason i am commiting now is becasue i messed up and i have to do some command z to go back and work off of an older version. this commiti s if that goes south

--- script.js
+++ script.js
@@ -14,33 +14,72 @@ function keyUp(ev)
     keys[ev.key] = false;
 }
 
+function moveLines()
+{
+    let roadLines = document.querySelectorAll('.line');
+    roadLines.forEach(function(item)
+    {
+        if (item.y >= 750)
+        {
+            item.y = item.y - 850;
+        }
+        item.y = item.y + 8;
+        item.style.top = item.y + 'px';
+    })
+}
+
+function moveEnemies(playerCar)
+{
+    let enemies =  document.querySelectorAll('.enemy');
+    playerCarBound = getBoundingClientRect();
+    enemies.forEach(function(item)
+    {
+        if (item.y > 750)
+        {
+            enemyCarBound = item.getBoundingClientRect();
+            if ((playerCarBound.bottom > enemyCarBound.top) ||  (playerCarBound.top > enemyCarBound.bottom) || (playerCarBound.left > enemyCarBound.right) || (playerCarBound.right > enemyCarBound.left))
+            {
+                console.log("YOU LOSE")
+            }
+            item.y = -300;
+            var leftEnemy = Math.floor(Math.random() * 350) + 'px'; 
+            item.style.left = leftEnemy; 
+            console.log(leftEnemy);
+        }
+        item.y = item.y + 7;
+        item.style.top = item.y + 'px';
+    })
+}
+
+
 function playArea()
 {
     let playerCar = document.querySelector('.car');
     let road = roadArea.getBoundingClientRect();
     if(player.start)
     {
-        if(keys.arrowUp && player.y > (road.top + 80))
+        moveLines();
+        moveEnemies(playerCar);
+
+        if(keys['ArrowUp'] && player.y > (road.top))
         {
             player.y = player.y - player.step;
         }
 
-        if(keys.arrowDown && player.y < (road.top - 80))
+        if(keys['ArrowDown'] && player.y < (road.bottom - 90))
         {
             player.y = player.y + player.step;
         }
 
-        if(keys.arrowLeft && player.x > 0)
+        if(keys['ArrowLeft'] && player.x > 0)
         {
             player.x = player.x - player.step;
         }
 
-        if(keys.arrowRight && player.x < (road.width - 64))
+        if(keys['ArrowRight'] && player.x < (road.width - 64))
         {
             player.x = player.x + player.step;
         }
-
-
 
         playerCar.style.top = player.y + 'px';
         playerCar.style.left = player.x + 'px';
@@ -60,8 +99,7 @@ function init()
     player.x = playerCar.offsetLeft;
     player.y = playerCar.offsetTop;
 
-
-    for (let x = 0; x < 5; x++)
+    for (let x = 0; x < 6; x++)
     {
         let roadLines = document.createElement('div');
         roadLines.setAttribute('class', 'line');
@@ -69,6 +107,20 @@ function init()
         roadLines.style.top = roadLines.y + 'px';
         roadArea.appendChild(roadLines)
     }
+
+    for (let x = 0; x < 5; x++)
+    {
+        let enemy = document.createElement('div');
+        enemy.setAttribute('class', 'enemy');
+        enemy.y = ((x + 1) * 300) * -1;
+        enemy.style.top = enemy.y + 'px';
+        enemy.style.left = Math.floor(Math.random() * 350) + 'px';
+        roadArea.appendChild(enemy);
+
+    }
+
+
+    
 
 }
 
